e522b873269f13d7d743a910153006d2b4cd2968
Travis McCord <TMAnimus@gmail.com>
2025-08-12 01:53:50 -0700

    Starting the project. Haven't consulted him yet.

---
+++ greggs.js
@@ -0,0 +1,5 @@
+// greggs.js
+console.log("greggs.js loaded successfully!");
+
+// You can add more interactive JavaScript here later,
+// for example, to handle time slot clicks or dynamic calendar features.
